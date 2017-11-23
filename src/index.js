6d67500b92b745f5d6a88c8c5be31a1d2ee0b92d
Nóra Szepes <nora.szepes@tenforce.com>
2017-11-23 15:45:31 +0100

    separated index.js from app.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// route
-import Router from './routers/router';
-import { BrowserRouter } from 'react-router-dom'
+import App from './app';
 
-// header
-import AppBar from 'material-ui/AppBar';
-
-// style
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {teal700, teal800, teal900} from 'material-ui/styles/colors'
-import './styles/default.scss';
-const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: teal900,
-    primary2Color: teal800,
-    primary3Color: teal700,
-    shadowColor: teal900
-  }
-});
-
-
-ReactDOM.render((<BrowserRouter>
-    <MuiThemeProvider muiTheme={muiTheme}>
-      <div>
-        <AppBar title="Board View" showMenuIconButton={false} />
-        <Router/>
-      </div>
-    </MuiThemeProvider>
-  </BrowserRouter>), document.getElementById('root'));
+ReactDOM.render((<App/>), document.getElementById('root'));
