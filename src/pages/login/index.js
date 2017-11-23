1a09768ddde4d1319439079504635bf5bd55414a
Nóra Szepes <nora.szepes@tenforce.com>
2017-11-23 11:56:47 +0100

    login card component

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,22 +2,26 @@ import React, { Component } from 'react';
 import './styles/index.scss';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 
 
 class Login extends Component {
   render() {
     return (
-      <div className="login">
-        <div className="login__box">
-          <div className="login__inputs">
-            <TextField floatingLabelText="username" />
-            <TextField floatingLabelText="password" />
-          </div>
-          <div className="login__actions">
-            <RaisedButton label='login' />
-          </div>
-        </div>
-      </div>
+
+
+       <Card className="login" >
+        <CardHeader title="Login" />
+        <CardText className="login__inputs">
+          <TextField className="login__input" floatingLabelText="username" />
+          <TextField className="login__input" floatingLabelText="password" type="password" />
+        </CardText>
+        <CardActions className="login__actions">
+          <RaisedButton label='login' />
+        </CardActions>
+      </Card>
     );
   }
 }
