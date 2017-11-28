ce91172a15bc30c0e4876e06b9c155be9af39089
Nóra Szepes <nora.szepes@tenforce.com>
2017-11-28 15:35:06 +0100

    added first version of non-table board without tickets

--- src/components/board-component/index.js
+++ src/components/board-component/index.js
@@ -1,70 +1,86 @@
 import React, { Component } from 'react';
 import './styles/index.scss';
 
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
+const statuses = {
+  1: "Status 1",
+  2: "Status 2",
+  3: "Status 3",
+  5: "Status 5",
+  6: "Status 6"
+};
+
 
 
 class BoardComponent extends Component {
   render() {
     return (
-       <Table
-         fixedHeader={true}
-         selectable={false}
-         multiSelectable={false}
-         >
-        <TableHeader
-          displaySelectAll={false}
-        >
-          <TableRow>
-            <TableHeaderColumn>User/Status</TableHeaderColumn>
-            <TableHeaderColumn>Status1</TableHeaderColumn>
-            <TableHeaderColumn>Status2</TableHeaderColumn>
-            <TableHeaderColumn>Status3</TableHeaderColumn>
-            <TableHeaderColumn>Status4</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody
-          displayRowCheckbox={false}
-          showRowHover={true}
-          stripedRows={true}
-        >
-          <TableRow>
-            <TableRowColumn><em>Unassigned</em></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>User1</TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>User2</TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>User3</TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-            <TableRowColumn></TableRowColumn>
-          </TableRow>
-        </TableBody>
-      </Table>
+       <div className="board-component">
+        <div className="board-component-header">
+          <div className="board-component-header-column">Person/Status</div>
+          <div className="board-component-header-column">Status 1</div>
+          <div className="board-component-header-column">Status 2</div>
+          <div className="board-component-header-column">Status 3</div>
+          <div className="board-component-header-column">Status 4</div>
+          <div className="board-component-header-column">Status 5</div>
+          <div className="board-component-header-column">Status 6</div>
+        </div>
+        <div className="board-component-body">
+          <div className="board-component-body-row">
+              <div className="board-component-body-row-element">Unassigned</div>
+              <div className="board-component-body-row-element">Box for Status 1</div>
+              <div className="board-component-body-row-element">Box for Status 2</div>
+              <div className="board-component-body-row-element">Box for Status 3</div>
+              <div className="board-component-body-row-element">Box for Status 4</div>
+              <div className="board-component-body-row-element">Box for Status 5</div>
+              <div className="board-component-body-row-element">Box for Status 6</div>
+          </div>
+          <div className="board-component-body-row">
+              <div className="board-component-body-row-element">Arnaud</div>
+              <div className="board-component-body-row-element">Box for Status 1</div>
+              <div className="board-component-body-row-element">Box for Status 2</div>
+              <div className="board-component-body-row-element">Box for Status 3</div>
+              <div className="board-component-body-row-element">Box for Status 4</div>
+              <div className="board-component-body-row-element">Box for Status 5</div>
+              <div className="board-component-body-row-element">Box for Status 6</div>
+          </div>
+          <div className="board-component-body-row">
+              <div className="board-component-body-row-element">Esteban</div>
+              <div className="board-component-body-row-element">Box for Status 1</div>
+              <div className="board-component-body-row-element">Box for Status 2</div>
+              <div className="board-component-body-row-element">Box for Status 3</div>
+              <div className="board-component-body-row-element">Box for Status 4</div>
+              <div className="board-component-body-row-element">Box for Status 5</div>
+              <div className="board-component-body-row-element">Box for Status 6</div>
+          </div>
+          <div className="board-component-body-row">
+              <div className="board-component-body-row-element">Martin</div>
+              <div className="board-component-body-row-element">Box for Status 1</div>
+              <div className="board-component-body-row-element">Box for Status 2</div>
+              <div className="board-component-body-row-element">Box for Status 3</div>
+              <div className="board-component-body-row-element">Box for Status 4</div>
+              <div className="board-component-body-row-element">Box for Status 5</div>
+              <div className="board-component-body-row-element">Box for Status 6</div>
+          </div>
+          <div className="board-component-body-row">
+              <div className="board-component-body-row-element">Nóra</div>
+              <div className="board-component-body-row-element">Box for Status 1</div>
+              <div className="board-component-body-row-element">Box for Status 2</div>
+              <div className="board-component-body-row-element">Box for Status 3</div>
+              <div className="board-component-body-row-element">Box for Status 4</div>
+              <div className="board-component-body-row-element">Box for Status 5</div>
+              <div className="board-component-body-row-element">Box for Status 6</div>
+          </div>
+          <div className="board-component-body-row">
+              <div className="board-component-body-row-element">Paul</div>
+              <div className="board-component-body-row-element">Box for Status 1</div>
+              <div className="board-component-body-row-element">Box for Status 2</div>
+              <div className="board-component-body-row-element">Box for Status 3</div>
+              <div className="board-component-body-row-element">Box for Status 4</div>
+              <div className="board-component-body-row-element">Box for Status 5</div>
+              <div className="board-component-body-row-element">Box for Status 6</div>
+          </div>
+        </div>
+       </div>
     );
   }
 }
