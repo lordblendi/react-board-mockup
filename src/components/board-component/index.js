23b58ac56e9f9a3df6678adaa5af9006df41819a
Nóra Szepes <nora.szepes@tenforce.com>
2017-11-29 13:49:54 +0100

    moved data to page level

--- src/components/board-component/index.js
+++ src/components/board-component/index.js
@@ -6,91 +6,7 @@ import BoardComponentRow from '../board-component-row/index';
 
 
 class BoardComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      statuses: [{
-          name: "Status 1",
-          id: 1
-        },
-        {
-          name: "Status 2",
-          id: 2
-        },
-        {
-          name: "Status 3",
-          id: 3
-        },
-        {
-          name: "Status 4",
-          id: 4
-        },
-        {
-          name: "Status 5",
-          id: 5
-        },
-        {
-          name: "Status 6",
-          id: 6
-        }
-      ],
-      users: [{
-        name: "Unassigned",
-        id: "Unassigned",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
-      }, {
-        name: "Arnaud",
-        id: "Arnaud",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
-      }, {
-        name: "Esteban",
-        id: "Esteban",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
-      }, {
-        name: "Martin",
-        id: "Martin",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
-      }, {
-        name: "Nóra",
-        id: "Nóra",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
-      }, {
-        name: "Paul",
-        id: "Paul",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
-      }]
-    };
-  }
+
 
 
   renderPerson(person) {
@@ -98,7 +14,7 @@ class BoardComponent extends Component {
     }
 
     listUsers() {
-      return this.state.users.map((person) => this.renderPerson(person));
+      return this.props.users.map((person) => this.renderPerson(person));
     }
 
     render() {
@@ -107,7 +23,7 @@ class BoardComponent extends Component {
           <div className = "board-component-header" >
             <div className = "board-component-header-column" > Person / Status </div>
             {
-              this.state.statuses.map(
+              this.props.statuses.map(
                 (status) =>
                 <div className = "board-component-header-column" key={status.id}>
                   {status.name}
--- src/pages/board/index.js
+++ src/pages/board/index.js
@@ -4,10 +4,96 @@ import BoardComponent from '../../components/board-component/index';
 
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      statuses: [{
+          name: "Status 1",
+          id: 1
+        },
+        {
+          name: "Status 2",
+          id: 2
+        },
+        {
+          name: "Status 3",
+          id: 3
+        },
+        {
+          name: "Status 4",
+          id: 4
+        },
+        {
+          name: "Status 5",
+          id: 5
+        },
+        {
+          name: "Status 6",
+          id: 6
+        }
+      ],
+      users: [{
+        name: "Unassigned",
+        id: "Unassigned",
+        1: "Box for Status 1",
+        2: "Box for Status 2",
+        3: "Box for Status 3",
+        4: "Box for Status 4",
+        5: "Box for Status 5",
+        6: "Box for Status 6"
+      }, {
+        name: "Arnaud",
+        id: "Arnaud",
+        1: "Box for Status 1",
+        2: "Box for Status 2",
+        3: "Box for Status 3",
+        4: "Box for Status 4",
+        5: "Box for Status 5",
+        6: "Box for Status 6"
+      }, {
+        name: "Esteban",
+        id: "Esteban",
+        1: "Box for Status 1",
+        2: "Box for Status 2",
+        3: "Box for Status 3",
+        4: "Box for Status 4",
+        5: "Box for Status 5",
+        6: "Box for Status 6"
+      }, {
+        name: "Martin",
+        id: "Martin",
+        1: "Box for Status 1",
+        2: "Box for Status 2",
+        3: "Box for Status 3",
+        4: "Box for Status 4",
+        5: "Box for Status 5",
+        6: "Box for Status 6"
+      }, {
+        name: "Nóra",
+        id: "Nóra",
+        1: "Box for Status 1",
+        2: "Box for Status 2",
+        3: "Box for Status 3",
+        4: "Box for Status 4",
+        5: "Box for Status 5",
+        6: "Box for Status 6"
+      }, {
+        name: "Paul",
+        id: "Paul",
+        1: "Box for Status 1",
+        2: "Box for Status 2",
+        3: "Box for Status 3",
+        4: "Box for Status 4",
+        5: "Box for Status 5",
+        6: "Box for Status 6"
+      }]
+    };
+  }
+
   render() {
     return (
        <div className="board">
-        <BoardComponent/>
+        <BoardComponent statuses={this.state.statuses} users={this.state.users}/>
       </div>
     );
   }
