1dd3089597628149c8eb57b4989a1ca6321fe6db
Nóra Szepes <nora.szepes@tenforce.com>
2017-11-29 13:52:11 +0100

    formatting code

--- src/components/board-component/index.js
+++ src/components/board-component/index.js
@@ -6,38 +6,34 @@ import BoardComponentRow from '../board-component-row/index';
 
 
 class BoardComponent extends Component {
-
-
-
   renderPerson(person) {
-    return ( <BoardComponentRow key={person.id} person={person} />);
-    }
-
-    listUsers() {
-      return this.props.users.map((person) => this.renderPerson(person));
-    }
-
-    render() {
-      return (
-        <div className = "board-component" >
-          <div className = "board-component-header" >
-            <div className = "board-component-header-column" > Person / Status </div>
-            {
-              this.props.statuses.map(
-                (status) =>
-                <div className = "board-component-header-column" key={status.id}>
-                  {status.name}
-                </div>
-              )
-            }
-
-          </div>
-          <div className = "board-component-body" >
-            {this.listUsers()}
-          </div>
-        </div>
-      );
-    }
+  return ( <BoardComponentRow key={person.id} person={person} />);
   }
 
-  export default BoardComponent;
+  listUsers() {
+    return this.props.users.map((person) => this.renderPerson(person));
+  }
+
+  render() {
+    return (
+      <div className = "board-component" >
+        <div className = "board-component-header" >
+          <div className = "board-component-header-column" > Person/Status </div>
+          {
+            this.props.statuses.map(
+              (status) =>
+              <div className = "board-component-header-column" key={status.id}>
+                {status.name}
+              </div>
+            )
+          }
+        </div>
+        <div className = "board-component-body" >
+          {this.listUsers()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BoardComponent;
