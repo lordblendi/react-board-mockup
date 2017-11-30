115047ebd420a14b4fb859cab45950e82cd6b1a8
Nóra Szepes <nora.szepes@tenforce.com>
2017-11-30 10:28:22 +0100

    new structure for data

--- src/pages/board/index.js
+++ src/pages/board/index.js
@@ -35,57 +35,143 @@ class Board extends Component {
       users: [{
         name: "Unassigned",
         id: "Unassigned",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
+        postits: [
+          {
+            statusType: 1,
+            title: "Esco Task1",
+            id: "1221"
+          },
+          {
+            statusType: 2,
+            title: "Esco Task2",
+            id: "1222"
+          },
+          {
+            statusType: 3,
+            title: "Esco Task3",
+            id: "1223"
+          },
+          {
+            statusType: 4,
+            title: "Esco Task4",
+            id: "1224"
+          }
+        ]
       }, {
         name: "Arnaud",
         id: "Arnaud",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
+        postits: [
+          {
+            statusType: 1,
+            title: "Oslo Task 1",
+            id: "1231"
+          },
+          {
+            statusType: 4,
+            title: "Oslo Task 4",
+            id: "1234"
+          },
+          {
+            statusType: 5,
+            title: "Oslo Task 5",
+            id: "1235"
+          },
+          {
+            statusType: 6,
+            title: "Oslo Task 6",
+            id: "1236"
+          }
+        ]
       }, {
         name: "Esteban",
         id: "Esteban",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
+        postits: [
+          {
+            statusType: 1,
+            title: "BDE Task 11",
+            id: "12411"
+          },
+          {
+            statusType: 1,
+            title: "BDE Task 12",
+            id: "12412"
+          },
+          {
+            statusType: 5,
+            title: "BDE Task 51",
+            id: "12451"
+          },
+          {
+            statusType: 5,
+            title: "BDE Task 52",
+            id: "12452"
+          }
+        ]
       }, {
         name: "Martin",
         id: "Martin",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
+        postits: [
+          {
+            statusType: 1,
+            title: "Eurostat Task 1",
+            id: "125"
+          },
+          {
+            statusType: 3,
+            title: "Eurostat Task 3",
+            id: "1253"
+          },
+          {
+            statusType: 2,
+            title: "Eurostat Task 2",
+            id: "1252"
+          },
+          {
+            statusType: 4,
+            title: "Eurostat Task 4",
+            id: "1254"
+          }
+        ]
       }, {
         name: "Nóra",
         id: "Nóra",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
+        postits: [
+          {
+            statusType: 1,
+            title: "Design Task 1",
+            id: "1261"
+          },
+          {
+            statusType: 2,
+            title: "Design Task 21",
+            id: "1262"
+          },
+          {
+            statusType: 4,
+            title: "Design Task 41",
+            id: "12641"
+          },
+          {
+            statusType: 4,
+            title: "Design Task 42",
+            id: "12642"
+          }
+        ]
       }, {
         name: "Paul",
         id: "Paul",
-        1: "Box for Status 1",
-        2: "Box for Status 2",
-        3: "Box for Status 3",
-        4: "Box for Status 4",
-        5: "Box for Status 5",
-        6: "Box for Status 6"
+        postits: [
+          {
+            statusType: 1,
+            title: "Powalco Task 1",
+            id: "1271"
+          },
+          {
+            statusType: 2,
+            title: "Powalco Task 2",
+            id: "1272"
+          }
+        ]
       }]
     };
   }
