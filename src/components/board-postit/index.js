16645e62557621a7df4f83d91070a07274c397c1
Nóra Szepes <nora.szepes@tenforce.com>
2017-11-30 14:13:45 +0100

    postit looks like postits now

--- src/components/board-postit/index.js
+++ src/components/board-postit/index.js
@@ -2,12 +2,16 @@ import React, {
   Component
 } from 'react';
 import './styles/index.scss';
+import Paper from 'material-ui/Paper';
 
 class BoardPostit extends Component {
 
   render() {
     return(
-      <div className="board-row-element-postit" >id: {this.props.postit.id}, title: {this.props.postit.title}</div>
+      <Paper className="board-postit" >
+        <div className="board-postit-id">{this.props.postit.id}</div>
+        <div className="board-postit-title">{this.props.postit.title}</div>
+      </Paper>
     );
   }
 }
